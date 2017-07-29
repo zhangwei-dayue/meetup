3bc978a78907ac6283102dd9d872c0ce4bd9f13c
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-29 08:01:58 +0800

    自动化测试登陆和退出成功

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "注册和登陆", :type => :feature do
   scenario "register" do
-    visit "users/sign_up"
+    visit "/users/sign_up"
 
     expect(page).to have_content("Sign up")
 
@@ -18,5 +18,28 @@ feature "注册和登陆", :type => :feature do
 
     user = User.last
     expect(user.email).to eq("spec@example.com")
+  end
+
+  scenario "login and logout" do
+    user = User.create!(:email => "spec@example.com", :password => "123456")
+
+    visit "/users/sign_in"
+
+    expect(page).to have_content("Log in")
+
+    within("#new_user") do
+      fill_in "Email", with: "spec@example.com"
+      fill_in "Password", with: "123456"
+    end
+
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
+
+    # click_link "Hi!,spec@example.com"
+    page.find(:xpath, "//a[@href='/users/sign_out']").click
+    # click_link "退出"
+
+    expect(page).to have_content("Signed out successfully")
   end
 end
