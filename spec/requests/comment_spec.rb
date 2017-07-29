5c9da3397d2e8e6de5caee003c43a34ea53a670c
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-29 18:14:59 +0800

    api测试不带auth_token和带有auth_token的发表评论案例

--- spec/requests/comment_spec.rb
+++ spec/requests/comment_spec.rb
@@ -30,4 +30,38 @@ RSpec.describe "API_V1::Comments", :type => :request do
     expect( Comment.count ).to eq(0)
   end
 
+  describe "发表评论" do
+    example "不带auth_token的成功案例" do
+      post "/api/v1/comments", :params => { :event_id => @event1.id, :name => "我要报名",
+                                          :body => "非常期待明天的meetup"}
+
+      expect(response).to have_http_status(200)
+
+      created_comment = Comment.last
+
+      expect(response.body).to eq( { :comment_url => api_v1_comment_url(created_comment.id)}.to_json )
+
+      expect(created_comment.name).to eq("我要报名")
+      expect(created_comment.body).to eq("非常期待明天的meetup")
+      expect(created_comment.user_id).to eq(nil)
+    end
+
+    example "带有auth_token的成功案例" do
+      post "/api/v1/comments", :params => { :auth_token => @user.authentication_token,
+                                            :event_id => @event1.id, :name => "我要报名",
+                                            :body => "非常期待明天的meetup"}
+
+      expect(response).to have_http_status(200)
+
+      created_comment = Comment.last
+
+      expect(response.body).to eq( { :comment_url => api_v1_comment_url(created_comment.id) }.to_json )
+
+      expect(created_comment.name).to eq("我要报名")
+      expect(created_comment.body).to eq("非常期待明天的meetup")
+      expect(created_comment.user_id).to eq(@user.id)
+    end
+
+  end
+
 end
